31e8e80f0376bde19f878afaeac178679fbbd4f4
J4cKr0y <Kiky270281@gmail.com>
2024-10-14 00:10:42 +0200

    refactor: upgrade marble style using tailwind.config

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -1,5 +1,6 @@
 //scr/app/components/Background.tsx
 import React from 'react';
+import Marbles from './Marbles'
 
 const Background: React.FC = () => {
   return (
@@ -11,7 +12,7 @@ const Background: React.FC = () => {
       
       <div data-name="Game" className="flex w-full h-full justify-center">
 
-        <div  data-name="Board" className="w-1/3 h-5/6 bg-gray-200 flex flex-col justify-around items-center">
+        <div  data-name="Board" className="w-1/4 h-5/6 bg-gray-200 flex flex-col justify-around items-center">
           {Array.from({ length: 10 }).map((_, rowIndex) => (
             <div key={`Line${rowIndex}`} className="flex justify-around w-full">
               {Array.from({ length: 4 }).map((_, colIndex) => (
@@ -20,15 +21,9 @@ const Background: React.FC = () => {
             </div>
           ))}
         </div>
-{/*<div className="w-8 h-8 bg-red-500 border border-red-400 rounded-full"></div>
-<div className="w-8 h-8 bg-blue-500 border border-blue-400 rounded-full"></div>
-<div className="w-8 h-8 bg-green-500 border border-green-400 rounded-full"></div>
-<div className="w-8 h-8 bg-yellow-500 border border-yellow-400 rounded-full"></div>
-<div className="w-8 h-8 bg-purple-500 border border-purple-400 rounded-full"></div>*/}
+
          <div data-name="Pick up zone" className="w-1/6 h-1/2 bg-gray-300 flex flex-col justify-around items-center ml-4">
-          {['red', 'purple', 'blue', 'green', 'yellow'].map((color, index) => (
-            <div key={`marble${index}`} data-name={`marble${index}`} className={`w-8 h-8 bg-${color}-500 border border-${color}-400 rounded-full`}></div>
-          ))}
+        <Marbles />
         </div> 
 
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,22 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+	    backgroundImage: {
+        'red-marble': 'radial-gradient(circle at 27% 27%, rgba(255, 205, 178, 1) 0%, rgba(255, 180, 162, 1) 25%, rgba(229, 152, 155, 1) 50%, rgba(181, 131, 141, 1) 75%, rgba(109, 104, 117, 1) 100%)',
+        'purple-marble': 'radial-gradient(circle at 27% 27%, rgba(229, 204, 255, 1) 0%, rgba(204, 153, 255, 1) 25%, rgba(178, 102, 255, 1) 50%, rgba(153, 51, 255, 1) 75%, rgba(127, 0, 255, 1) 100%)',
+        'blue-marble': 'radial-gradient(circle at 27% 27%, rgba(178, 205, 255, 1) 0%, rgba(162, 180, 255, 1) 25%, rgba(155, 152, 229, 1) 50%, rgba(141, 131, 181, 1) 75%, rgba(117, 104, 109, 1) 100%)',
+        'green-marble': 'radial-gradient(circle at 27% 27%, rgba(178, 255, 205, 1) 0%, rgba(162, 255, 180, 1) 25%, rgba(155, 229, 152, 1) 50%, rgba(141, 181, 131, 1) 75%, rgba(109, 117, 104, 1) 100%)',
+        'yellow-marble': 'radial-gradient(circle at 27% 27%, rgba(255, 255, 178, 1) 0%, rgba(255, 255, 162, 1) 25%, rgba(229, 229, 155, 1) 50%, rgba(181, 181, 141, 1) 75%, rgba(117, 117, 109, 1) 100%)',
+        'marble-before': 'radial-gradient(circle at 50% 0px, #ffffff, rgba(255, 255, 255, 0) 58%)',
+        'marble-after': 'radial-gradient(circle at 50% 50%, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8) 14%, rgba(255, 255, 255, 0) 24%)',
+      },
+      boxShadow: {
+        'inner-marble': 'inset 1px 1px 2px rgba(0, 0, 0, 0.3), inset -1px -1px 2px rgba(255, 255, 255, 0.5)',
+        'outer-marble': '1px 1px 3px rgba(0, 0, 0, 0.3)',
+      },
     },
   },
+  variants: {},
   plugins: [],
 };
 export default config;
